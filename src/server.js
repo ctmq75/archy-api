c6b613cf4689ee6cb1a117b2aa40fbde12d1d0db
Chris McArdle <christopher.mcardle2@gmail.com>
2020-07-29 10:54:37 -0500

    another commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
-const app = require('./app')
 
-const { PORT } = require('./config')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello worlddd')
 })
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
