e3a81effc9db2ddfc0746374a55d644a74943b48
Chris McArdle <christopher.mcardle2@gmail.com>
2020-08-11 18:45:45 -0500

    fixes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
   module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: 5432,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/archy-app',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
