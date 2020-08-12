d708afe2212c3b1ceb333b0de2c438d304f299ae
Chris McArdle <christopher.mcardle2@gmail.com>
2020-08-12 07:31:11 -0500

    fixes

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
-  "ssl": !!process.env.SSL,
+  "connectionString": 'postgres://zovtduicxjtess:7c9c066bd7fd3782f8576262a81fe14fd07025270c8943c2ddcfdcd6c9873877@ec2-34-195-115-225.compute-1.amazonaws.com:5432/d8f3004n1adorn',
   "validateChecksums": false,
 
 };
